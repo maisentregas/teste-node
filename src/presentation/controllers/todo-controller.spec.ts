df8b22c6ca852036824beb0bcf056f5fc19d09d4
FelipeFreitas96 <felipe.samidare@hotmail.com>
2020-12-04 06:45:22 -0300

    test: Deveria criar um Todo se os parametros estiverem válidos

--- src/presentation/controllers/todo-controller.spec.ts
+++ src/presentation/controllers/todo-controller.spec.ts
@@ -1,25 +1,77 @@
 import { TodoController } from './todo-controller';
+import { AddTodoModel, DeleteTodoModel, Todo, UpdateTodoModel } from '../../domain/usecases/todo';
+import { TodoModel } from '../../domain/models/todo';
+
+const makeSut = () => {
+    class TodoStub implements Todo {
+        list(): Promise<Array<TodoModel>> {
+            return Promise.resolve([{
+                id: -1,
+                description: 'any_description',
+                created_at: -1,
+            }, {
+                id: 0,
+                description: 'any_description',
+                created_at: 0,
+            }]);
+        }
+        add(addTodoModel: AddTodoModel): Promise<TodoModel> {
+            return Promise.resolve({
+                id: -1,
+                description: 'any_description',
+                created_at: -1,
+            });
+        }
+        update(updateTodoModel: UpdateTodoModel): Promise<TodoModel> {
+            return Promise.resolve({
+                id: -1,
+                description: 'updated_description',
+                created_at: -1,
+            });
+        }
+        delete(deleteTodoModel: DeleteTodoModel): Promise<Boolean> {
+            return Promise.resolve(true);
+        }
+    }
+
+    const todoStub = new TodoStub();
+    const sut = new TodoController(todoStub);
+    return { sut, todoStub };
+}
 
 describe('Todo-Controller', () => {
     test('Deveria retornar o erro 500 ao tentar criar Todo inválido', async () => {
-        const sut = new TodoController();
+        const { sut } = makeSut();
         const request = {
             body: {},
         };
 
-        const response = await sut.handle(request);
+        const response = await sut.handle('POST', request);
         expect(response.statusCode).toBe(500);
     });
 
     test('Deveria retornar o erro 200 ao criar Todo válido', async () => {
-        const sut = new TodoController();
+        const { sut } = makeSut();
         const request = {
             body: {
                 description: 'any_description',
             },
         };
 
-        const response = await sut.handle(request);
+        const response = await sut.handle('POST', request);
         expect(response.statusCode).toBe(200);
+    });
+
+    test('Deveria criar um Todo se os parametros estiverem válidos', async () => {
+        const { sut, todoStub } = makeSut();
+        const addSpy = jest.spyOn(todoStub, 'add');
+        const request = {
+            body: {
+                description: 'any_description',
+            },
+        };
+
+        await sut.handle('POST', request);
+        expect(addSpy).toHaveBeenCalledWith({ description: 'any_description' });
     });
 });
