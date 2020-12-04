96a129512e7beda471d58d46a7125bebc1aeabad
FelipeFreitas96 <felipe.samidare@hotmail.com>
2020-12-04 05:40:13 -0300

    feat: função list adicionada aos casos de uso

--- src/domain/usecases/todo.ts
+++ src/domain/usecases/todo.ts
@@ -14,6 +14,7 @@ export interface DeleteTodoModel {
 }
 
 export interface Todo {
+    list(): Promise<Array<TodoModel>>;
     add(addTodoModel: AddTodoModel): Promise<TodoModel>;
     update(updateTodoModel: UpdateTodoModel): Promise<TodoModel>;
     delete(deleteTodoModel: DeleteTodoModel): Promise<Boolean>;
