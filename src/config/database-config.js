0d577a2a05051d2c2c372b74f2879da4e099b593
Caio Felipe <devcaiofelipe15@gmail.com>
2020-12-08 18:56:05 -0300

    Configurando express rotas e porta do servidor

--- src/config/database-config.js
+++ src/config/database-config.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
+
 module.exports = {
-  username: 'root',
-  password: 'docker',
-  database: 'blog',
-  host: '127.0.0.1',
-  port: 3306,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   dialect: 'mysql',
   define: {
     timestamps: true,
