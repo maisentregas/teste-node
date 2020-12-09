4b86c41ca0a14cf41e127154b76b9ec5c322c2bb
Caio Felipe <devcaiofelipe15@gmail.com>
2020-12-09 10:31:53 -0300

    Atualizar tasks finalizado

--- src/app/controllers/task-controller.js
+++ src/app/controllers/task-controller.js
@@ -18,4 +18,30 @@ export default new class TaskController {
 
     return res.json({ id, task, done });
   };
+  
+
+  async update(req, res) {
+    const taskData = Yup.object().shape({
+      id: Yup.number().required(),
+      task: Yup.string().max(255),
+      done: Yup.boolean()
+    });
+    
+    if(!(await taskData.isValid(req.body))) {
+      return res.status(400).json({ error: 'Task id must be sendly or data types are incorrect' });
+    };
+
+    const taskExists = await Task.findOne({ where: { id: req.body.id }});
+
+    if(!taskExists) {
+      return res.json({ error: "Task not found" })
+    };
+
+    const { id, task, done } = await taskExists.update(req.body, {
+      where: { id: req.body.id }
+    });
+
+
+    return res.json({ id, task, done });
+  };
 };
--- src/routes.js
+++ src/routes.js
@@ -5,5 +5,6 @@ import TaskController from './app/controllers/task-controller';
 const routes = new Router();
 
 routes.post('/task/create', TaskController.store);
+routes.put('/task/update/', TaskController.update);
 
 export default routes;
