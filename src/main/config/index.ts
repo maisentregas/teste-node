f64d2c0085f35a62dec37a87f2e0caf10d8a535f
FelipeFreitas96 <felipe.samidare@hotmail.com>
2020-12-10 06:53:41 -0300

    feat: Composição do Express

---
+++ src/main/config/index.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+import { useRoutes } from './routes';
+import { useMiddlewares } from './middlewares';
+
+const app = express();
+useMiddlewares(app);
+useRoutes(app);
+
+export default app;
