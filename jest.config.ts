b70a1b77b131c6b0d40be10e2f16f1dbe84574f9
FelipeFreitas96 <felipe.samidare@hotmail.com>
2020-12-11 09:12:59 -0300

    chore: Jest não fazer coverage da pasta src/main

--- jest.config.ts
+++ jest.config.ts
@@ -20,7 +20,10 @@ export default {
   // collectCoverage: false,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  // collectCoverageFrom: undefined,
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+  ],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: "coverage",
